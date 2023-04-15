6308386a7dc850897be277af2d32b3e25719b1be
Dmitry Shakirov <117525799+DShakirov@users.noreply.github.com>
2023-04-15 14:07:12 +0300

    Add files via upload

--- carsstat/templatetags/most_popular.py
+++ carsstat/templatetags/most_popular.py
@@ -10,7 +10,7 @@ def most_popular(cnt=5):
     models = Generation.objects.raw(
             'SELECT gen.id, gen.name, COUNT(*) AS counter, model.name AS carmodel, model.id AS carmodel_id, mark.name AS markname FROM '
             'carsstat_car AS car JOIN carsstat_generation AS gen ON car.generation_id = gen.id JOIN carsstat_carmodel '
-            'AS model ON car.car_model_id = model.id JOIN carsstat_mark AS mark ON car.mark_id=mark.id'
+            'AS model ON car.car_model_id = model.id JOIN carsstat_mark AS mark ON car.mark_id=mark.id '
             'GROUP BY gen.id ORDER BY COUNT(*) DESC'
                                     )[:cnt]
     return {'models':models}
