5c5b9b6be12df5c4de37b053b1ce2bd70f51ff55
Dmitry Shakirov <117525799+DShakirov@users.noreply.github.com>
2023-04-18 19:04:24 +0300

    Add files via upload

--- carsstat/views.py
+++ carsstat/views.py
@@ -173,7 +173,7 @@ class GetPopularCarModels(ListView):
         object_list = Generation.objects.raw(
             'SELECT gen.id, gen.name, COUNT(*) AS counter, model.name AS carmodel, model.id AS carmodel_id, mark.name AS markname FROM '
             'carsstat_car AS car JOIN carsstat_generation AS gen ON car.generation_id = gen.id JOIN carsstat_carmodel '
-            'AS model ON car.car_model_id = model.id JOIN carsstat_mark AS mark ON car.mark_id=mark.id'
+            'AS model ON car.car_model_id = model.id JOIN carsstat_mark AS mark ON car.mark_id=mark.id '
             'GROUP BY gen.id, model.id, mark.id ORDER BY COUNT(*) DESC'
                                     )
         return object_list
