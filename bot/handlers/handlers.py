bcf32afc82cfc629685c87a922c010f0fcd3ec33
Dmitry Shakirov <117525799+DShakirov@users.noreply.github.com>
2023-04-18 19:03:23 +0300

    Add files via upload

--- bot/handlers/handlers.py
+++ bot/handlers/handlers.py
@@ -204,25 +204,25 @@ def secret_level(update, context): #callback_data: SECRET_LEVEL_BUTTON variable 
     )
 
 
-#def broadcast_decision_handler(update, context): #callback_data: CONFIRM_DECLINE_BROADCAST variable from manage_data.py
-   # """ Entered /broadcast <some_text>.
-   #     Shows text in Markdown style with two buttons:
-   #     Confirm and Decline
-   # """
-  #  broadcast_decision = update.callback_query.data[len(md.CONFIRM_DECLINE_BROADCAST):]
-  #  entities_for_celery = update.callback_query.message.to_dict().get('entities')
-  #  entities = update.callback_query.message.entities
-   # text = update.callback_query.message.text
-  #  if broadcast_decision == md.CONFIRM_BROADCAST:
-   #     admin_text = st.msg_sent,
-   #     user_ids = list(User.objects.all().values_list('user_id', flat=True))
-  #      broadcast_message.delay(user_ids=user_ids, message=text, entities=entities_for_celery)
-  #  else:
-  #      admin_text = text
+def broadcast_decision_handler(update, context): #callback_data: CONFIRM_DECLINE_BROADCAST variable from manage_data.py
+    """ Entered /broadcast <some_text>.
+        Shows text in Markdown style with two buttons:
+        Confirm and Decline
+    """
+    broadcast_decision = update.callback_query.data[len(md.CONFIRM_DECLINE_BROADCAST):]
+    entities_for_celery = update.callback_query.message.to_dict().get('entities')
+    entities = update.callback_query.message.entities
+    text = update.callback_query.message.text
+    if broadcast_decision == md.CONFIRM_BROADCAST:
+        admin_text = st.msg_sent,
+        user_ids = list(User.objects.all().values_list('user_id', flat=True))
+        broadcast_message.delay(user_ids=user_ids, message=text, entities=entities_for_celery)
+    else:
+        admin_text = text
 
-  #  context.bot.edit_message_text(
-   #     text=admin_text,
-   #     chat_id=update.callback_query.message.chat_id,
-   #     message_id=update.callback_query.message.message_id,
-  #      entities=None if broadcast_decision == md.CONFIRM_BROADCAST else entities
-  #  )
+    context.bot.edit_message_text(
+        text=admin_text,
+        chat_id=update.callback_query.message.chat_id,
+        message_id=update.callback_query.message.message_id,
+        entities=None if broadcast_decision == md.CONFIRM_BROADCAST else entities
+    )
